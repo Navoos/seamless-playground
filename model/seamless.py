a0387769deb021f5c028c9f9bf6a16ba6cc738b9
Yaakoub Khoudrouf <yakhoudr@e1r12p2.1337.ma>
2024-02-16 11:30:20 +0100

    release

--- model/seamless.py
+++ model/seamless.py
@@ -14,11 +14,11 @@ def translate_audio(audio_file_fd, target):
 	with open("/Users/yakhoudr/goinfre/seamless-playground/" + audio_file_fd.name, "wb+") as destination:
 		for chunk in audio_file_fd.chunks():
 			destination.write(chunk)
+	convert_to_wav("/Users/yakhoudr/goinfre/seamless-playground/" + audio_file_fd.name, "/Users/yakhoudr/goinfre/seamless-playground/" + "wav_format.wav")
 	audio, orig_freq =  torchaudio.load("/Users/yakhoudr/goinfre/seamless-playground/" + "wav_format.wav")
 	audio =  torchaudio.functional.resample(audio, orig_freq=orig_freq, new_freq=16_000) # must be a 16 kHz waveform array
 	audio_inputs = processor(audios=audio, return_tensors="pt")
 	audio_inputs = audio_inputs.to(device)
-	convert_to_wav("/Users/yakhoudr/goinfre/seamless-playground/" + audio_file_fd.name, "/Users/yakhoudr/goinfre/seamless-playground/" + "wav_format.wav")
 	audio_array_from_audio = model.generate(**audio_inputs, tgt_lang=target)[0].cpu().numpy().squeeze()
 	audio_tensor = torch.from_numpy(audio_array_from_audio).float()
 	if audio_tensor.ndim == 1:
